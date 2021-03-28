055f0bf7f4d3675cedb4d7fadacdf2ad52a254b2
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-03-29 07:57:04 +1000

    else if instead of if chain and author/description

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -6,20 +6,18 @@ run: (client, message, args) => {
 const target = message.mentions.users.first() || message.author;
 
 const fields = [
-    {name: `Username`, value:`${target.username}`, inline: true}
+    {name: `Roles`, value:`${target.member.roles.cache.size}`, inline: true}
 ]
 
 if (target.id == 817653964161548289) {
     fields.push(
         {name: `Acknowledgements`, value: `The Manami Bot`}
     )
-}
-if (target.id == 537117477721604096) {
+} else if (target.id == 537117477721604096) {
     fields.push(
         {name: `Acknowledgements`, value: `Manami Bot Developer`}
     )
-}
-if (target.id == 712170999222632469) {
+} else if (target.id == 712170999222632469) {
     fields.push(
         {name: `Acknowledgements`, value: `Manami Bot Developer`}
     )
@@ -28,6 +26,8 @@ if (target.id == 712170999222632469) {
 const targetEmbed = new MessageEmbed()
 .setTitle(`${target.username}`)
 .setURL(target.displayAvatarURL({ dynamic: true, size: 1024 }))
+.setAuthor(`${target.username}`, target.displayAvatarURL({ dynamic: true, size: 1024 }))
+.setDescription(target)
 .addFields(fields)
 
 message.channel.send(targetEmbed)
