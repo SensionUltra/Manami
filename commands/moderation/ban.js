8155609edacaa283a69ed64e7577796e7f709b77
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-03-28 10:27:53 +1000

    kick and ban are done

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -7,13 +7,16 @@ module.exports = {
     run: (client, message, args) => {
         const target = message.mentions.members.first(); 
         let reason = args.slice(1).join(" "); 
+        
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(`you do not have the required permissions to use this command ${message.author}`)
+        
+        if (!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send('i do not have permissions to ban members')
+        
+        if (!target.bannable) return message.channel.send(`i can not ban ${target.user.username}, this is becuse either they have a role that is above mine or they are the owner of this server`)
+        
+        if(target.id == message.author.id) return message.channel.send('You can\'t ban yourself')
 
-        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply(`you do not have the required permissions to use this command <@${message.author.id}>`)
-
-        if (target == message.author.id) return message.channel.send('You can\'t ban yourself')
-
-
-        if (target == message.author.id) return message.channel.send('Cant ban yourself')
+        if(target.id == message.author.id) return message.channel.send('Cant ban yourself')
 
         if (!args[0]) return message.channel.send('Specify the member to ban')
 
--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -7,12 +7,16 @@ module.exports = {
     run: (client, message, args) => {
         const target = message.mentions.members.first(); 
         let reason = args.slice(1).join(" "); 
+        
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(`you do not have the required permissions to use this command ${message.author}`)
+        
+        if (!message.guild.me.hasPermission('KICK_MEMBERS')) return message.channel.send('i do not have permissions to kick members')
+        
+        if (!target.kickable) return message.channel.send(`i can not kick ${target.user.username}, this is becuse either they have a role that is above mine or they are the owner of this server`)
+        
+        if(target.id == message.author.id) return message.channel.send('You can\'t kick yourself')
 
-        if (!message.author.hasPermission('KICK_MEMBERS')) return message.channel.send(`you do not have the required permissions to use this command ${message.author}`)
-
-        if(target == message.author.id) return message.channel.send('You can\'t kick yourself')
-
-        if(target == message.author.id) return message.channel.send('Cant kick yourself')
+        if(target.id == message.author.id) return message.channel.send('Cant kick yourself')
 
         if (!args[0]) return message.channel.send('Specify the member to kick')
 
@@ -26,6 +30,6 @@ module.exports = {
         .addField(`kicked by`, `${message.author}`)
         .addField(`reason`, `${reason}`)
         message.channel.send(kickedEmbed)
-        target.kick(reas)
+        target.kick(reason)
     }
 }
