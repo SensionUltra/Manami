8061ef64645330487eb51d359cb0c5deb21a85a5
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-03-28 12:19:47 +1000

    fixed the say command to delete the trigger message

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -3,6 +3,7 @@ name: "say",
 description: "makes the bot say what you want it to say",
 run: (client, message, args) => {
     const toSay = args.join(' ')
-        message.channel.send(toSay)
+    message.channel.send(toSay)
+    message.delete()
     }
 }
