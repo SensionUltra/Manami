5995d3d0b5fe41560ec2dd9270d9366c10377669
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-03-29 20:06:14 +1000

    Updated the whois command: List of roles now working shows user created at and joined at time added a thumbnail, footer and timestamp to the embed

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -1,12 +1,18 @@
 const { MessageEmbed } = require("discord.js");
+const moment = require('moment');
 module.exports = {
 name: "whois",
 description: "gets info about a user",
 run: (client, message, args) => {
 const target = message.mentions.users.first() || message.author;
+const targetMember = message.mentions.members.first() || message.member;
+const joinDiscord = moment(target.createdAt).format('llll');
+const joinServer = moment(target.joinedAt).format('llll');
 
 const fields = [
-    {name: `Roles`, value: `testing`, inline: true}
+    {name: `Created At`, value: `${joinDiscord}`, inline: true},
+    {name: `Joined At`, value: `${joinServer}`, inline: true},
+    {name: `Roles`, value: targetMember.roles.cache.map(r => `${r}`).join(' | ')},
 ]
 
 switch (true) {
@@ -25,17 +31,15 @@ case (target.id == 537117477721604096):
             {name: `Acknowledgements`, value: `Manami Bot Developer`}
     )
 }
-
-
-
-
-
 const targetEmbed = new MessageEmbed()
 .setTitle(`${target.username}`)
 .setURL(target.displayAvatarURL({ dynamic: true, size: 1024 }))
 .setAuthor(`${target.username}`, target.displayAvatarURL({ dynamic: true, size: 1024 }))
+.setThumbnail(target.displayAvatarURL({ dynamic: true, size: 1024 }))
 .setDescription(target)
 .addFields(fields)
+.setFooter(`ID: ${target.id}`)
+.setTimestamp()
 
 message.channel.send(targetEmbed)
 }
