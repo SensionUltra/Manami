af5547ff92727d4c83c55c59d426aa85a4b35bdb
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-03-29 16:00:06 +1000

    changed the if else to a swich statement

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -6,22 +6,32 @@ run: (client, message, args) => {
 const target = message.mentions.users.first() || message.author;
 
 const fields = [
-    {name: `Roles`, value:`${target.member.roles.cache.size}`, inline: true}
+    {name: `Roles`, value: `testing`, inline: true}
 ]
 
-if (target.id == 817653964161548289) {
+switch (true) {
+case (target.id == 817653964161548289):
+    console.log(`the bot`)
     fields.push(
         {name: `Acknowledgements`, value: `The Manami Bot`}
     )
-} else if (target.id == 537117477721604096) {
+break;
+case (target.id == 537117477721604096):
+    console.log(`zac`)
     fields.push(
         {name: `Acknowledgements`, value: `Manami Bot Developer`}
     )
-} else if (target.id == 712170999222632469) {
-    fields.push(
-        {name: `Acknowledgements`, value: `Manami Bot Developer`}
+    break;
+    case (target.id == 712170999222632469):
+        console.log(`awoken`)
+        fields.push(
+            {name: `Acknowledgements`, value: `Manami Bot Developer`}
     )
 }
+
+
+
+
 
 const targetEmbed = new MessageEmbed()
 .setTitle(`${target.username}`)
