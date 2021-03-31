a460965023bf5b2460f113396646459ef979e6f7
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-03-31 10:11:09 +1000

    added a way to see all the options to modify text. when the user does not give any option it will give all the options to modify the text. if the user does give a option but not no text to modify it will notify the user

--- commands/fun/modify.js
+++ commands/fun/modify.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 module.exports = {
 name: "modify",
 description: "modified text with the options",
@@ -6,17 +7,29 @@ run: (client, message, args) => {
 const option = args[0]
 const text = args.slice(1).join(' ')
 
+const optionsList = [
+    {name: `gap`, value: `Adds a space inbetween each charicter`, inline: true},
+    {name: `reverse`, value: `Reverses the text`, inline: true},
+]
+const options = new MessageEmbed()
+.setTitle(`Options To Modify Text`)
+.addFields(optionsList)
+
 switch (option) {
     case 'gap':
+        if (!text) return message.channel.send(`you need to provide text to modify!`)
         const spacedAndJoined = text.split('').join(' ')
         message.channel.send(`${spacedAndJoined}`)
-    break;
-    case 'reverse':
-        const reversed = text.split('').reverse().join('')
-        message.channel.send(reversed)
-    break;
-    default: 
-    message.channel.send('you need to provide a option')
+        
+        break;
+        case 'reverse':
+            if (!text) return message.channel.send(`you need to provide text to modify!`)
+            const reversed = text.split('').reverse().join('')
+            message.channel.send(reversed)
+                break;
+                default: 
+                if (!option) return message.channel.send(options)
+    message.channel.send('That is not a valid option')
 }
 }
 }
