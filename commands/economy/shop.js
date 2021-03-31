2cc4cc7451d96e23942a516697eb664ceb9a1af6
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-03-31 19:15:42 +1000

    started working on a shop

---
+++ commands/economy/shop.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const economy = require('../../economy');
+module.exports = {
+name: "shop",
+description: "buy stuff from the shop",
+run: async(client, message, args) => {
+    const item = args[0]
+    if (!item) return message.channel.send('you need to give me a item to buy')
+    const guildId = message.guild.id
+    const userId = message.author.id
+    const usersCoins = await economy.getCoins(guildId, userId)
+    fs.readFile('./shop.json', 'utf8', (err, data) => {
+        if (err) {
+            message.channel.send('there was a error executing that cmd, could you please try again later')
+            return console.log(err)
+        } else {
+            const items = JSON.parse(data);
+            items.forEach( async(obj) => {
+                if (obj.name != item) return
+
+                const coinsPreNegative = obj.price
+                const newCoins = await economy.takeCoins(guildId, userId, coinsPreNegative)
+
+                message.channel.send(`congratulations, you bought ${obj.name} for ${obj.price}, now you have ${newCoins}`)
+            });
+        }
+    })
+}
+}
