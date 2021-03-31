bb368469faac5a085e165d2149ef0d45abde6487
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-03-31 19:38:58 +1000

    updated the shop cmd now has more messages and checks for errors

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 const fs = require('fs')
 const economy = require('../../economy');
 module.exports = {
@@ -9,6 +10,7 @@ run: async(client, message, args) => {
     const guildId = message.guild.id
     const userId = message.author.id
     const usersCoins = await economy.getCoins(guildId, userId)
+    if (usersCoins <= 0) return message.channel.send('you dont have any money to buy anything!')
     fs.readFile('./shop.json', 'utf8', (err, data) => {
         if (err) {
             message.channel.send('there was a error executing that cmd, could you please try again later')
@@ -18,10 +20,11 @@ run: async(client, message, args) => {
             items.forEach( async(obj) => {
                 if (obj.name != item) return
 
+                if (obj.price > usersCoins) return message.channel.send(`you do not have enough money to buy this item, you would need ${obj.price} coins and you have ${usersCoins} coins!`)
                 const coinsPreNegative = obj.price
                 const newCoins = await economy.takeCoins(guildId, userId, coinsPreNegative)
 
-                message.channel.send(`congratulations, you bought ${obj.name} for ${obj.price}, now you have ${newCoins}`)
+                message.channel.send(`congratulations, you bought ${obj.name} for ${obj.price} coins, now you have ${newCoins}`)
             });
         }
     })
