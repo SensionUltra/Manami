d777a8bdbffe45783a83b117b612b15930788fce
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-04-01 16:44:20 +1000

    new file with functions for pre-made embeds you will need to always pass message at the end of it tho

---
+++ embeds.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = (client) => {}
+
+module.exports.error = (title, errMessage, message) => {
+    const errorEmbed = new MessageEmbed()
+    .setTitle(title)
+    .setDescription(errMessage)
+    .setColor('#ff0000')
+    return message.channel.send(errorEmbed)
+}
+
+module.exports.succes = (title, succesMessage, message) => {
+    const succesEmbed = new MessageEmbed()
+    .setTitle(title)
+    .setDescription(succesMessage)
+    .setColor('#00ff00')
+    return message.channel.send(succesEmbed)
+}
+
+module.exports.fieldListEmbed = (title, fields, message) => {
+    const fieldListEmbed = new MessageEmbed()
+    .setTitle(title)
+    .addFields(fields)
+    .setColor('#00ff00')
+    return message.channel.send(fieldListEmbed)
+}
