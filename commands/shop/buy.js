c970bc663a42602366569d313964bc8d0f89e952
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-04-02 08:15:33 +1000

    added a lot more comments to the buy cmd

--- commands/shop/buy.js
+++ commands/shop/buy.js
@@ -7,38 +7,25 @@ name: "buy",
 description: "buy stuff from the shop",
 run: async(client, message, args) => {
     const itemArgs = args.join(' ')
-    if (!itemArgs) return embed.error('MIssing Arguments', 'you need to give me a item to buy', message)
+    if (!itemArgs) return embed.error('Missing Arguments', 'you need to give me a item to buy', message) // checks whether the user gave the right amount of args
     const guildId = message.guild.id
     const userId = message.author.id
-    let noItemCheckArray = []; // this should be one, and gets pushed when the bot finds the right item
-    let noItemCheck = 0 // if there is no item this should be 0
-    let noItemChecking = []; // this should be the amount of items, and should be done before we start
-    const usersCoins = await economy.getCoins(guildId, userId)
-    if (usersCoins <= 0) return embed.error('you dont have any money to buy anything!')
-    fs.readFile('./shop.json', 'utf8', (err, data) => {
+    const usersCoins = await economy.getCoins(guildId, userId) // checks how many coins the user has
+    if (usersCoins <= 0) return embed.error('Not Enough Coins', 'you dont have any money to buy anything!', message) // checks if the user has 0 or less coins and if so then tells the user
+    fs.readFile('./shop.json', 'utf8', (err, data) => { // reads from the file that stors the items
         if (err) {
-            embed.error('Command Failed', 'there was a error executing that cmd, could you please try again later', message)
+            embed.error('Command Failed', 'there was a error executing that cmd, could you please try again later', message) // tells the user if there was a error reading from the file becuse that can happen
             return console.log(err)
         } else {
-            const items = JSON.parse(data);
-            items.forEach(obj => {
-                noItemChecking.push(
-                    obj.name
-                )
-                    });
+            const items = JSON.parse(data); // parses the data from the readfile
             items.forEach( async(obj) => {
-                noItemCheckArray.push(
-                    obj.name
-                )
-                if (obj.name != itemArgs) return
+                if (obj.name != itemArgs) return //  checks whether this is the right item to buy
+                if (obj.price > usersCoins) return embed.error('Not Enough Money', `you do not have enough money to buy this item, you would need ${obj.price} coins and you have ${usersCoins} coins!`, message) //  if the user dosnt have enough coins it will tell the user how many coins they need
                 
-                if (obj.price > usersCoins) return embed.error('Not Enough Money', `you do not have enough money to buy this item, you would need ${obj.price} coins and you have ${usersCoins} coins!`, message)
-                const item = obj
-                const newCoins = await shop.buyItem(guildId, userId, item)
-                noItemCheck++
-                embed.succes('Congratulations!', `you bought ${obj.name} for ${obj.price} coins, now you have ${newCoins}`, message)
+                const item = obj // defines the object with a nicer name
+                const newCoins = await shop.buyItem(guildId, userId, item) // buys the item from the shop
+                embed.succes('Congratulations!', `you bought ${obj.name} for ${obj.price} coins, now you have ${newCoins}`, message) // tells the user info about what they bought
             });
-            if (noItemChecking.length == noItemCheckArray.length && noItemCheck == 0) return embed.error('Not A Item', 'that is not a existing item', message)
         }
     })
 }
