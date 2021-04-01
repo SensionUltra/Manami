d92f6ce2ce728a7f976752da02b72becab384ec4
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-04-01 16:46:09 +1000

    updated to use embeds.js not working right now for a unknown reason i am looking into that though

--- commands/shop/buy.js
+++ commands/shop/buy.js
@@ -1,33 +1,35 @@
-const { MessageEmbed } = require("discord.js");
 const fs = require('fs')
 const economy = require('../../economy');
 const shop = require('../../shop')
+const embed = require('../../embeds')
 module.exports = {
 name: "buy",
 description: "buy stuff from the shop",
 run: async(client, message, args) => {
-    const itemArgs = args[0]
-    if (!itemArgs) return message.channel.send('you need to give me a item to buy')
+    const itemArgs = args.join(' ')
+    if (!itemArgs) return embed.error('MIssing Arguments', 'you need to give me a item to buy', message)
     const guildId = message.guild.id
     const userId = message.author.id
     const usersCoins = await economy.getCoins(guildId, userId)
-    if (usersCoins <= 0) return message.channel.send('you dont have any money to buy anything!')
+    let noItem = true;
+    if (usersCoins <= 0) return embed.error('you dont have any money to buy anything!')
     fs.readFile('./shop.json', 'utf8', (err, data) => {
         if (err) {
-            message.channel.send('there was a error executing that cmd, could you please try again later')
+            embed.error('Command Failed', 'there was a error executing that cmd, could you please try again later', message)
             return console.log(err)
         } else {
             const items = JSON.parse(data);
             items.forEach( async(obj) => {
                 if (obj.name != itemArgs) return
 
-                if (obj.price > usersCoins) return message.channel.send(`you do not have enough money to buy this item, you would need ${obj.price} coins and you have ${usersCoins} coins!`)
-                const coinsPreNegative = obj.price
+                if (obj.price > usersCoins) return embed.error('Not Enough Money', `you do not have enough money to buy this item, you would need ${obj.price} coins and you have ${usersCoins} coins!`, message)
                 const item = obj
                 const newCoins = await shop.buyItem(guildId, userId, item)
 
-                message.channel.send(`congratulations, you bought ${obj.name} for ${obj.price} coins, now you have ${newCoins}`)
+                noItem = false
+                embed.succes('Congratulations!', `you bought ${obj.name} for ${obj.price} coins, now you have ${newCoins}`, message)
             });
+            if (noItem) return embed.error('Not A Item', 'that is not a existing item', message)
         }
     })
 }
