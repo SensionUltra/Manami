65fbddefe248a09fb4fbce6eaacd53748ad61665
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-04-01 20:16:39 +1000

    trying to get it to check if a item exists or not right now it checks befor it finishes reading the array

--- commands/shop/buy.js
+++ commands/shop/buy.js
@@ -10,8 +10,10 @@ run: async(client, message, args) => {
     if (!itemArgs) return embed.error('MIssing Arguments', 'you need to give me a item to buy', message)
     const guildId = message.guild.id
     const userId = message.author.id
+    let noItemCheckArray = []; // this should be one, and gets pushed when the bot finds the right item
+    let noItemCheck = 0 // if there is no item this should be 0
+    let noItemChecking = []; // this should be the amount of items, and should be done before we start
     const usersCoins = await economy.getCoins(guildId, userId)
-    let noItem = true;
     if (usersCoins <= 0) return embed.error('you dont have any money to buy anything!')
     fs.readFile('./shop.json', 'utf8', (err, data) => {
         if (err) {
@@ -19,17 +21,24 @@ run: async(client, message, args) => {
             return console.log(err)
         } else {
             const items = JSON.parse(data);
+            items.forEach(obj => {
+                noItemChecking.push(
+                    obj.name
+                )
+                    });
             items.forEach( async(obj) => {
+                noItemCheckArray.push(
+                    obj.name
+                )
                 if (obj.name != itemArgs) return
-
+                
                 if (obj.price > usersCoins) return embed.error('Not Enough Money', `you do not have enough money to buy this item, you would need ${obj.price} coins and you have ${usersCoins} coins!`, message)
                 const item = obj
                 const newCoins = await shop.buyItem(guildId, userId, item)
-
-                noItem = false
+                noItemCheck++
                 embed.succes('Congratulations!', `you bought ${obj.name} for ${obj.price} coins, now you have ${newCoins}`, message)
             });
-            if (noItem) return embed.error('Not A Item', 'that is not a existing item', message)
+            if (noItemChecking.length == noItemCheckArray.length && noItemCheck == 0) return embed.error('Not A Item', 'that is not a existing item', message)
         }
     })
 }
