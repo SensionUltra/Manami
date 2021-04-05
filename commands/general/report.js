c194f2ed9b49cd31fbf291c21472b89fba58eeec
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-04-05 16:39:46 +1000

    updated report now has charicter limits and a confermation message

--- commands/general/report.js
+++ commands/general/report.js
@@ -1,24 +1,38 @@
+const embed = require('@auto/embeds')
 const { MessageEmbed } = require("discord.js")
+const reportMax = 1500 // the maximim amout of charicters in a report
 
 module.exports = {
 name: "report",
 description: "report a bug from anyserver!",
 run: (client, message, args) => {
 
-    const channel = client.channels.cache.get("827075339980111925")
+    const channel = client.supportServer.reportChannel
 
     const reportMsg = args.join(' ')
 
-    if(!reportMsg) {
-        message.channel.send("Please type something to report")
-    }
+    if(!reportMsg) return embed.error('Missing Arguments', "You need to give me something to report", message)
 
-    const embed = new MessageEmbed()
+    embed.succes('Succesfully Sent Your Report To The Devs', `Your report "${reportMsg}", has now been sent to the devs, you will get a dm inbetween now and 2 weeks with information about your report`, message).catch(err => {
+        if (err || reportMsg.length > reportMax) {
+            embed.error('Could Not Send Your Report', 'Could not send your report, this is becuse it was to big', message)
+            console.log(err)
+        }
+    })
+if (reportMsg.length > reportMax) {
+    let reportServer = client.supportServer
+    reportServer.channel = reportServer.reportChannel
+    embed.error('Report To Big', `The report sent by ${message.author.username}, is to big to send`, reportServer)
+    return
+} else {
+
+    const reportEmbed = new MessageEmbed()
     .setTitle(`<:siren_ez:827804301576896552> New Report! <:siren_ez:827804301576896552>`)
     .setDescription(reportMsg)
     .setFooter(`Sent By ${message.author.username} In Guild: ${message.guild.name}`)
     .setTimestamp()
-
-    channel.send(embed)
+    
+    channel.send(reportEmbed)
+}
 }
 }
