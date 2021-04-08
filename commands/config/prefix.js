9fd119b8f8e8fe03368ef07c88fa8b5cff818ce6
ScienceyZac <78137526+ScienceyZac@users.noreply.github.com>
2021-04-08 18:55:26 +1000

    prefix now automataclyl updates

--- commands/config/prefix.js
+++ commands/config/prefix.js
@@ -8,7 +8,7 @@ run: async(client, message, args) => {
     const guildId = message.guild.id
     const prefix = args.join(' ') || 'm.'
     const newPrefix = await guild.setPrefix(guildId, prefix)
-    guild.prefix = prefix
+    message.guild.prefix = prefix
     embed.succes('Succesfully Changed The Server Prefix', `Succesfully changed the server prefix to ${newPrefix}`, message)
 }
 }
