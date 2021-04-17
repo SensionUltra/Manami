b8a88655fd29e66250770b3130d24a322e461bb9
Awoken <72582948+SensionUltra@users.noreply.github.com>
2021-04-16 21:49:25 -0700

    Added Guild Leave logs

---
+++ events/guild/guildDelete.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: 'guildDelete',
+    run: async(guild, client) => {
+
+
+		const onLeaveChannel = client.channels.cache.get("832838373081219072")
+
+		const onLeaveEmbed  = new MessageEmbed()
+		.setTitle(":cry: Server Left :cry:")
+		.addField("Guild", "```" + guild.name + "```")
+		.addField("Server Region", "```" + guild.region + "```", true)
+		.addField("Guild ID", "```" + guild.id + "```", true)
+		.addField("Guild Owner ID", "```" + guild.ownerID + "```", true)
+		.addField("Guild Members", "```" + guild.memberCount + "```", true)
+
+		
+
+		onLeaveChannel.send(onLeaveEmbed)
+
+    }}
