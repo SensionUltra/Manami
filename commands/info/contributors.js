a1fac87cb62d168dcfc4ccb6b132af89afbae77f
Mahir Molai <mahir@molai.dev>
2021-06-17 15:49:47 +0500

    Create a contributors command
    However, Awoken (@SensionUltra) will have to add their github api key to the config.json which will be named apiToken, you can get it from Settings >> Developer >> API Keys, be sure to give it the repos management permissions, you won't need anything other than that.

---
+++ commands/info/contributors.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const config = require("../../config.json");
+
+module.exports = {
+  name: "contributors",
+  description: "List the contributors for Manami",
+  run: async (client, message, args) => {
+    var contributors = await (
+      await fetch("https://api.github.com/repos/SensionUltra/Manami/contributors", {
+        method: "GET",
+        headers: {
+          Authorization: `token ${config.apiToken}`,
+        },
+      })
+    ).json();
+
+    contributors = contributors.map(
+      (contributor) =>
+        new Object({
+          name: contributor.login,
+          value: `[Click me to visit their GitHub](${contributor.html_url})`,
+          inline: true,
+        })
+    );
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle("All the contributors that are helping make Manami awesome!")
+      .setDescription(
+        "Be sure to drop a star on the repo, [here!](https://github.com/SensionUltra/Manami)"
+      )
+      .addFields(contributors)
+      .setColor("BLUE")
+      .setFooter(`Manami - This command was made by molai.dev#9999!`);
+
+    message.inlineReply(embed);
+  },
+};
