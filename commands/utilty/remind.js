cb7ff390a4049e037516444dd86f4b31d0772b08
Scienceyzac <scienceyzac@gmail.com>
2021-06-17 19:49:04 +1000

    fixed remind cmd

--- commands/utilty/remind.js
+++ commands/utilty/remind.js
@@ -5,26 +5,45 @@ name: "remind",
 aliases: [""],
 description: "set a reminder!",
 run: (client, message, args) => {
-    let time = args[0]
-
+    let matches = args.join(' ').match(/"(.+)" ?, ?"(\d+)" ?, ?"(s|m|h|d|y)"/i)
+if (!matches)return message.channel.send('invalid style, try doing: \n"msg to be reminded","time","in seconds, minutes, etc, and do it with the first charicter, e.g. s"');
+let timeTimed;
+switch (matches[3]) {
+    case 's':
+        timeTimed = 1000
+        break;
+    case 'm':
+        timeTimed = 60000
+    break;
+    case 'h':
+        timeTimed = 3.6000E+6
+    break;
+    case 'd':
+        timeTimed = 86400000
+    break;
+    case 'y':
+        timeTimed = 3.1536E+10
+    break;
+}
+let time = matches[2] * timeTimed
     if (!time) {
         return message.inlineReply("`❌` Please provide a time!");
     }
 
-    const reminder = args.join(" ").slice(args[0].length);
+    const reminder = matches[1]
 
     const remindEmbed = new MessageEmbed()
         .setTitle("`✅` Remind Set")
         .addField("Reminder:", `${reminder}`)
-        .addField("Timeout", `${time}`)
+        .addField("Timeout", `${matches[2]} ${matches[3]}`)
 
     message.channel.send(remindEmbed)
 
-    setTimeout(function () {
+    setTimeout(()=> {
         const finRemindEmbed = new MessageEmbed()
             .setTitle("`🚨` Reminder!!!")
             .setDescription(`**${reminder}**`)
 
         message.member.send(finRemindEmbed)
-    }, ms(time))
+    }, time)
 }}
