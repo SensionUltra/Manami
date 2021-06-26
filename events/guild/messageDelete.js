c2cf55ce414cd768edd6f19c31397ce81422b9c8
Scienceyzac <scienceyzac@gmail.com>
2021-06-27 09:43:02 +1000

    stoped unknown error

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -2,8 +2,8 @@ module.exports = {
   name: "messageDelete",
   run: async (message) => {
 
-      if (message.author.bot) return;
-      const snipes = message.client.snipes.get(message.channel.id) || [];
+      if (message.author?.bot) return;
+      const snipes = message.client.snipes?.get(message.channel.id) || [];
       snipes.unshift({
         content: message.content,
         author: message.author,
